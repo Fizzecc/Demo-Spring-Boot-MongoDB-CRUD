e72f8c979655a60f8b583a2794b143d0fb4eff8c
Fizzecc <fizzecc@gmail.com>
2020-05-05 13:46:31 +0800

    initial commit

---
+++ src/main/java/com/example/mongodb/demo/repository/TutorialRepository.java
@@ -0,0 +1,12 @@
+package com.example.mongodb.demo.repository;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.example.mongodb.demo.model.Tutorial;
+
+public interface TutorialRepository extends MongoRepository<Tutorial, String>{
+    List<Tutorial> findByTitleContaining(String title);
+    List<Tutorial> findByPublished(boolean published);
+}
